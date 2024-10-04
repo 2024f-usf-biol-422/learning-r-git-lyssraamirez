b5c63382c5cf8decb566c1eeaa3a3dc85b14ea0f
Alyssa Ramirez <amramirez18@dons.usfca.edu>
2024-10-04 13:03:20 -0700

    Filter (subset data, sample ID) return filter subset

--- snp_functions_argument.R
+++ snp_functions_argument.R
@@ -7,4 +7,15 @@ library(dplyr)
 #path
 snp_path <- "data/sample_snp_data.csv"
 
+#apply filter by snp_path,ssample from sample_snp_data.csv file of a single row
+subset_snp_by_sample_id <- function(snp_path,ssample){
+  snp_data <- read.csv(snp_path)
+  snp_data %>%
+    filter(ssample == "
+SRR12433063")
+}
+#return the dplyr filter subset of snp_path
+return(subset_snp_by_sample_id(snp_path,"SRR12433063"))
+
+
 

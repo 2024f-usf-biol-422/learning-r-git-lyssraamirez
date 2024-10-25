3e753e4b71caa44eab21efc74ab5e8db8637e12c
Alyssa Ramirez <amramirez18@dons.usfca.edu>
2024-10-25 14:40:03 -0700

    Lint and load packages

---
+++ code/SNP_analyzation.R
@@ -0,0 +1,20 @@
+###Alyssa Ramirez
+###amramirez18@dons.usftca.edu
+###10/24/24
+
+# subset_snp_data.R
+# This script defines a function to subset a SNP dataset by a given sample ID. Chatgpt was used as a guide.
+# The function takes two arguments:
+#   - file_path: A string with the path/name of the SNP data set CSV file.
+#   - sample_id: A string with the sample ID to subset by.
+# The function returns a data frame containing only rows for the specified sample ID.
+
+
+#load packages
+library(dplyr)
+library(lintr)
+lint("data/sample_snp_data.csv")
+
+#Define function
+
+

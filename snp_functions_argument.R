24952d03518ed2c8d595ed5bab3f14e666f5bd54
Alyssa Ramirez <amramirez18@dons.usfca.edu>
2024-10-25 13:48:32 -0700

    Load Packages

--- snp_functions_argument.R
+++ snp_functions_argument.R
@@ -5,18 +5,7 @@
 library(dplyr)
 library(lintr)
 
-#path
-snp_path <- "data/sample_snp_data.csv"
 
-#apply filter by snp_path,ssample from sample_snp_data.csv file of a single row
-subset_snp_by_sample_id <- function(snp_path,ssample){
-  snp_data <- read.csv(snp_path)
-  snp_data %>%
-    filter(ssample == "
-SRR12433063")
-}
-#return filtered data set
-return(subset_snp_by_sample_id(snp_path,"SRR12433063"))
 
 
 

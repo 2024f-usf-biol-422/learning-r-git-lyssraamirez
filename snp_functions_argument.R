0f2b05c1d3d5cc7ee2665afb9bd5692e11483a5c
Alyssa Ramirez <amramirez18@dons.usfca.edu>
2024-10-25 14:03:32 -0700

    Load packages

--- snp_functions_argument.R
+++ snp_functions_argument.R
@@ -1,9 +1,13 @@
-#Alyssa Ramirez
-#amramirez18@dons.usfca.edu
+##Alyssa Ramirez
+##amramirez18@dons.usfca.edu
+## 10/24/24
 
 #load packages
 library(dplyr)
 library(lintr)
+
+#define function
+
 
 
 

a83105ca08279f46eae09b518938277463137f6d
Alyssa Ramirez <amramirez18@dons.usfca.edu>
2024-10-28 11:06:56 -0700

    Rscript bash

--- SNP_bash_report.sh
+++ SNP_bash_report.sh
@@ -10,4 +10,7 @@ if [ "$#" -ne 2 ]; then
 fi
 sample_id="$2"
 snp_data= "$1"
+Rscript -e "rmarkdown::render(REPORT.Rmd',
+           params = list(snp_data = '$snp_data', sample_id = '$SAMPLE_ID'),
+           output_file = paste0('output/', '$SAMPLE_ID', '_REPORT.html'))"
 

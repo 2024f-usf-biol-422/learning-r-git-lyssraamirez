64c4bfb66950b3006d7884a7e647323462f21e3d
Alyssa Ramirez <amramirez18@dons.usfca.edu>
2024-10-28 11:00:15 -0700

    Parameters set and checked

--- SNP_bash_report.sh
+++ SNP_bash_report.sh
@@ -1,3 +1,13 @@
 #Alyssa Ramirez
 #amramirez18@dons.usfca.edu
 #10/26/24
+#!/bin/bash
+set -euo pipefail
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: ./render_report.sh path/data/sample_snp_data copy.csv"
+  exit 1
+fi
+sample_id="$2"
+snp_data= "$1"
+

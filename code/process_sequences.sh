4709a26e5fa01175faaede29e31f445c000f23e2
amramirez18@dons.usfca.edu <amramirez18@dons.usfca.edu>
2024-11-18 05:36:07 +0000

    Make sure files are created

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-#Make good use of defensive programming and echo if  command line argument is >1.
-if ["$#" -ne 1 ]; then
-echo  "Use of : $0  <path_for_fasta.gz_data_file>
+# Make good use of defensive programming and echo if  command line argument is >1.
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <path_to_fasta.gz_file>"
+  exit 1
+fi
+
+fi
+
+# Make sure file is created
+if [ ! -f "$1" ]; then
+  echo "Error: File '$1' not found!"
+  exit 1
+fi
 

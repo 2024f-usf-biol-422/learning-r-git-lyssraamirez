0b2d7682ac25544928585f8b9098ddfa2eee57da
amramirez18@dons.usfca.edu <amramirez18@dons.usfca.edu>
2024-11-18 06:02:33 +0000

    Deleted 10 lines; startover

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -8,20 +8,3 @@ fi
 
 fi
 
-# Make sure file is created
-if [ ! -f "$1" ]; then
-  echo "Error: File '$1' not found!"
-  exit 1
-fi
-
-# Calculate total number of sequences
-echo "Calculate the total number of sequences in the dataset"
-total_sequences=$(zgrep -c "^>" "$1")
-echo "Total sequences: $total_sequences"
-
-# Count sequences by country
-echo "Tallying sequences by country"
-zgrep "^>" "$1" | \
-  awk -F'|' '{print $3}' | \
-  sort | uniq -c | sort -nr > country_sequence_counts.txt
-echo "Country tallies saved to country_sequence_counts.txt."

ef0c948e497278bbce947ca46a4adc0437ebd985
Alyssa Ramirez <amramirez18@dons.usfca.edu>
2024-11-20 17:03:21 -0800

    She bang moved

--- SNP_bash_report.sh
+++ SNP_bash_report.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+
 #Alyssa Ramirez
 #amramirez18@dons.usfca.edu
 #10/26/24
-#!/bin/bash
-set -euo pipefail
 
+set -euo pipefail
 if [ "$#" -ne 2 ]; then
   echo "Usage: ./render_report.sh path/to/SNP_data.csv sampleID"
   exit 1
